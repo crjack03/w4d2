1699bdd028ff588e3eb8b4bf8a25ae6317fbf18e
Christopher Jackson <Chris2003jackson@gmail.com>
2022-01-26 08:46:24 -0500

    push

---
+++ app.js
@@ -0,0 +1,70 @@
+var myAddFunction = 1 + 2;
+console.log(myAddFunction(1,2));
+var myAddFunction = 3 + 4;
+console.log(myAddFunction(3,4));
+var myAddFunction = 5 + 6;
+console.log(myAddFunction(5,6));
+
+const cat = {
+  "name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "enemies": ["Water", "Dogs"]
+};
+const anotherObject = {
+  make: "Ford",
+  5: "five",
+  "model": "focus"
+};
+const myDog = {
+    // Only change code below this line
+  "name": Riley,
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Milo", "Moka"]
+    // Only change code above this line
+
+  };
+  // Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  // Only change code below this line
+  const hatValue = testObj;      
+  const hat = val1;
+const shirtValue = testObj;    
+const shirt = val2;
+
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj;  
+  const entreeValue = entree;
+  const drinkValue = testObj;     
+  const drinkValue = drink;
+
+  const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark = "woof";
+
+  // Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.name = "Happy Coder";
+
+  
